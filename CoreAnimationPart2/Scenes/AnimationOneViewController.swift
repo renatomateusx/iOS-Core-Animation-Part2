330844c9faade453cabdf57e9bab21e1b1827f63
Renato Mateus <renatomateusx@gmail.com>
2021-06-01 10:06:25 -0300

    Fist Animation

---
+++ CoreAnimationPart2/Scenes/AnimationOneViewController.swift
@@ -0,0 +1,81 @@
+//
+//  AnimationOneViewController.swift
+//  CoreAnimationPart2
+//
+//  Created by Renato Mateus on 01/06/21.
+//
+
+import UIKit
+
+class AnimationOneViewController: UIViewController {
+  
+  
+  //MARK: Properties
+  
+  var spriteImages = [UIImage]()
+  
+  private lazy var label: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 15, weight: .bold)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = "Click on the heart image"
+    label.textAlignment = .center
+    return label
+  }()
+  
+  lazy var image: UIImageView = {
+    let image = UIImageView()
+    image.isUserInteractionEnabled = true
+    image.image = UIImage(named: "tile00")
+    image.translatesAutoresizingMaskIntoConstraints = false
+    image.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.animate)))
+    return image
+  }()
+  
+  
+  
+  //MARK: Lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    configureTabBar()
+    configureImages()
+    configureView()
+  }
+  
+  func configureTabBar(){
+    navigationController?.navigationBar.isHidden = false
+  }
+  func configureView(){
+    view.backgroundColor = .white
+    view.addSubview(label)
+    view.addSubview(image)
+    
+    label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+    label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10).isActive = true
+    label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10).isActive = true
+    label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    
+    
+    image.heightAnchor.constraint(equalToConstant: 100).isActive = true
+    image.widthAnchor.constraint(equalToConstant: 100).isActive = true
+    image.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    image.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+  }
+  
+  func configureImages(){
+    for i in 0 ..< 29 {
+      spriteImages.append(UIImage(named: "tile0\(i)")!)
+    }
+  }
+  
+  
+  //MARK: Helpers
+  @objc func animate(){
+    image.animationImages = spriteImages
+    image.animationDuration = 0.6
+    image.animationRepeatCount = 1
+    image.startAnimating()
+  }
+}
--- CoreAnimationPart2/ViewController.swift
+++ CoreAnimationPart2/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 class ViewController: UIViewController {
   // MARK: Properties
   
+  private lazy var label: UILabel = {
+    let label = UILabel()
+    label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+    label.translatesAutoresizingMaskIntoConstraints = false
+    label.text = "Animation List"
+    label.textAlignment = .center
+    return label
+  }()
+  
+  private lazy var scroll: UIScrollView = {
+    let scroll = UIScrollView()
+    scroll.backgroundColor = .clear
+    scroll.translatesAutoresizingMaskIntoConstraints = false
+    return scroll
+  }()
+  
+  private lazy var btnOne:UIButton = {
+    let button = createButton(title: "Animation One")
+    button.addTarget(self, action: #selector(animationOneTapped), for: .touchUpInside)
+    return button
+  }()
+  
   // MARK: Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -24,7 +46,54 @@ class ViewController: UIViewController {
     navigationController?.navigationBar.barStyle = .black
   }
   func configureView(){
+    view.backgroundColor = .white
     
+    view.addSubview(label)
+    view.addSubview(scroll)
+    scroll.addSubview(btnOne)
+    
+    NSLayoutConstraint.activate([
+      label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+      label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+      label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
+      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      
+      scroll.topAnchor.constraint(equalTo: label.bottomAnchor),
+      scroll.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+      scroll.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+      scroll.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+      
+      
+      
+      btnOne.topAnchor.constraint(equalTo: scroll.topAnchor, constant: 20),
+      btnOne.leadingAnchor.constraint(equalTo: scroll.leadingAnchor, constant: 10),
+      btnOne.trailingAnchor.constraint(equalTo: scroll.trailingAnchor, constant: -10),
+      btnOne.heightAnchor.constraint(equalToConstant: 42),
+      btnOne.centerXAnchor.constraint(equalTo: scroll.centerXAnchor),
+      
+    ])
+  }
+  
+  func createButton(title:String? = nil, image:UIImage? = nil) -> UIButton{
+    let button = UIButton(type: .custom)
+    button.setTitleColor(.white, for: .normal)
+    button.titleLabel?.font = .systemFont(ofSize: 15, weight: .bold)
+    button.backgroundColor = .lightGray
+    button.layer.cornerRadius = 5
+    if let title = title {
+      button.setTitle(title, for: .normal)
+    }
+    if let image = image {
+      button.setImage(image, for: .normal)
+    }
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }
+  
+  
+  // MARK: Selectors
+  @objc func animationOneTapped(){
+    navigationController?.pushViewController(AnimationOneViewController(), animated: true)
   }
   
 }
